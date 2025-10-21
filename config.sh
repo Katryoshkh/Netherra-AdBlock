ee1eee07065bd802e29992835bfa91dc45a08034
Katryoshkh <riyadifarras@gmail.com>
2025-10-21 11:44:40 +0700

    Create config.sh for Netherra AdBlock module
    Initialize script for Netherra AdBlock module with configuration settings and module info.

--- config.sh
+++ config.sh
@@ -1,1 +1,25 @@
+#!/system/bin/sh
+# MODULENAME: Netherra AdBlock
+MODID=netherraadblock
 
+# Skip mount on certain folders (no need for us)
+SKIPMOUNT=false
+
+# Set to true if you want to load properties or system scripts
+PROPFILE=false
+POSTFSDATA=false
+LATESTARTSERVICE=false
+
+# Print module info to Magisk log
+print_modname() {
+  ui_print "*******************************"
+  ui_print "   Netherra AdBlock v1.0      "
+  ui_print "        by AX271              "
+  ui_print "*******************************"
+}
+
+# Permissions setting (optional here)
+set_permissions() {
+  # Default permission: read-only for hosts file
+  set_perm $MODPATH/system/etc/hosts 0 0 644
+}
